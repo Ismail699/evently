62a5741e7b3877cbde4f641a564dc324a3a6ca76
John Doe <ismailmarouani20@gmail.com>
2024-04-21 01:35:04 +0200

    new erversion

--- middleware.ts
+++ middleware.ts
@@ -1,27 +1,21 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  debug: true,
   publicRoutes: [
+    "/api/webhooks(.*)",
     "/",
     "/events/:id",
     "/api/webhook/clerk",
     "/api/webhook/stripe",
     "/api/uploadthing",
-    "/assets/images/logo.svg",
-    "/assets/images/dotted-pattern.png",
   ],
   ignoredRoutes: [
     "/api/webhook/clerk",
     "/api/webhook/stripe",
     "/api/uploadthing",
-    "/assets/images/logo.svg",
-    "/assets/images/dotted-pattern.png",
-    "/favicon.ico",
-    "/assets/images/hero.png",
   ],
 });
 
 export const config = {
-  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
